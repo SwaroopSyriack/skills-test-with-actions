44d1d959a0b7ab1c197afc5a15340138a1c8b527
SwaroopSyriack <168578192+SwaroopSyriack@users.noreply.github.com>
2026-09-25 06:38:21 +0000

    making the changes to make 55

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -59,7 +59,7 @@ def test_get_nth_fibonacci_one():
 
 
 def test_get_nth_fibonacci_ten():
-    """Test with n=10. and made a change to do test commit"""
+    """Test with n=10. and made a change to do test commit changing the value to 55 """
     # Arrange
     n = 10
 
